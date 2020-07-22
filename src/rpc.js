115f3dfcc2f1f714f517b9c7e5728cf6c4b8ab89
like <likeaixi@gmail.com>
2020-07-22 09:43:45 +0800

    rpc: fix genPayload return value type

--- src/rpc.js
+++ src/rpc.js
@@ -3,20 +3,21 @@ const request = require("xhr-request-promise");
 const genPayload = (() => {
   let nextId = 0;
   return (method, params) => ({
-    jsonrpc: "2.0",
-    id: ++nextId,
-    method: method,
-    params: params
+    "jsonrpc": "2.0",
+    "id": ++nextId,
+    "method": method,
+    "params": params
   });
 })();
 
 const send = url => (method, params) => {
   return request(url, {
     method: "POST",
-    contentType: "application/json-rpc",
-    body: JSON.stringify(genPayload(method,params))
+    contentType: "application/json",
+    json: "json",
+    body: genPayload(method,params)
   }).then(answer => {
-    var resp = JSON.parse(answer); // todo: use njsp?
+    var resp = answer; // todo: use njsp?
     if (resp.error) {
       throw new Error(resp.error.message);
     } else {
